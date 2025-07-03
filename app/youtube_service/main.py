923b0edc1d621dc07ffa10cc97b171bc033a4d6d
EC2 Default User <ec2-user@ip-172-31-23-2.ec2.internal>
2025-07-03 02:41:58 +0000

    feat : add cors

--- app/youtube_service/main.py
+++ app/youtube_service/main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from youtube_service.search.routers.youtube_search import router as youtube_router
 
 app = FastAPI()
+
+# ? CORS Çã¿ë
+origins = [
+    "http://34.228.65.221:3000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(youtube_router, prefix="/youtube")
 
 @app.get("/")
 def root():
     return {"message": "Hello from youtube_service!"}
+
+
+for route in app.routes:
+    print(f"? {route.path}")
