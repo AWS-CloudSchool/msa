ff7495eccb0b4347ab4d4c75b897fd4454b80dcc
EC2 Default User <ec2-user@ip-172-31-23-2.ec2.internal>
2025-07-04 04:54:44 +0000

    add /analyze in report-service

--- app/report_service/main.py
+++ app/report_service/main.py
@@ -4,7 +4,7 @@ from report_service.analyze.routers.youtube_analyze import router as analysis_ro
 from report_service.audio.routers.audio_service import router as audio_router
 from report_service.s3.routers.s3 import router as report_router
 #from report_service.routers.s3 import router as s3_router
-#from report_service.routers.user_analysis import router as user_analysis_router
+from routers.user_analysis import router as user_analysis_router
 
 app = FastAPI()
 
@@ -24,6 +24,8 @@ app.include_router(analysis_router, prefix="/analysis")
 app.include_router(audio_router, prefix="/audio")
 
 app.include_router(report_router, prefix="/report")
+app.include_router(analysis_router, prefix="/analyze")
+
 #app.include_router(s3_router, prefix="/s3")
 #app.include_router(user_analysis_router, prefix="/user_analysis")
 
